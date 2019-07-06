3070badaebc4ea60ec2d30d2e5e9799f60a12d70
Alexandre d'Alton <alex@alexdalton.org>
2019-07-06 19:44:39 +0200

    create update.sh script
    Signed-off-by: Alexandre d'Alton <alex@alexdalton.org>

---
+++ update.sh
@@ -0,0 +1,7 @@
+Massdrop Loader 1.03
+
+Massdrop Loader  Copyright (C) 2018  Massdrop Inc.
+This program is Free Software and has ABSOLUTELY NO WARRANTY
+
+Opening port '/dev/cu.usbmodem1433201'... Failed! (No such file or directory)
+Error: Could not open port! (Correct port?)
